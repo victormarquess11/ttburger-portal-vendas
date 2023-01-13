705212388ac8df8f60a347db2606705692fe35dd
Victor Marques Ribeiro <victormarquess11@gmail.com>
2023-01-13 16:45:57 -0300

    [FIX] api url

--- frontend/src/components/TableSales.vue
+++ frontend/src/components/TableSales.vue
@@ -14,57 +14,67 @@ export default {
   data() {
     return {
       sales: [],
-    }
+    };
   },
   mounted() {
-    fetch('http://localhost:5000/vendas')
-      .then(res=>res.json())
-      .then(data => this.sales = data)
-      .catch(err => console.log(err.message));
+    fetch("http://localhost/api/vendas")
+      .then((res) => res.json())
+      .then((data) => (this.sales = data))
+      .catch((err) => console.log(err.message));
   },
   computed: {
     totalSales() {
-      if (this.sales[0]){
-        let onlySales = this.sales.map(item => Number(item.valor_total));
-        let sum = onlySales.reduce((prev, next)=> Number(prev) + Number(next))
+      if (this.sales[0]) {
+        let onlySales = this.sales.map((item) => Number(item.valor_total));
+        let sum = onlySales.reduce((prev, next) => Number(prev) + Number(next));
         return this.formatToNumber(sum);
       }
       return "0.00";
     },
     totalTargetSales() {
-      if (this.sales[0]){
-        let onlyTargetSales = this.sales.map(item => Number(item.meta_valor));
-        let sum = onlyTargetSales.reduce((prev, next)=> Number(prev) + Number(next));
+      if (this.sales[0]) {
+        let onlyTargetSales = this.sales.map((item) => Number(item.meta_valor));
+        let sum = onlyTargetSales.reduce(
+          (prev, next) => Number(prev) + Number(next)
+        );
         return this.formatToNumber(sum);
       }
       return "0.00";
     },
     totalProductsPerSale() {
-      if (this.sales[0]){
-        let onlyProductsSold = this.sales.map(item => Number(item.qtd_produtos)/Number(item.qtd_vendas));
-        let sum = onlyProductsSold.reduce((prev, next)=> Number(prev) + Number(next)).toString();
-        return this.formatToNumber(sum/onlyProductsSold.length);
+      if (this.sales[0]) {
+        let onlyProductsSold = this.sales.map(
+          (item) => Number(item.qtd_produtos) / Number(item.qtd_vendas)
+        );
+        let sum = onlyProductsSold
+          .reduce((prev, next) => Number(prev) + Number(next))
+          .toString();
+        return this.formatToNumber(sum / onlyProductsSold.length);
       }
       return "0.00";
     },
     totalAverageTicket() {
-      if (this.sales[0]){
-        let onlyAverageTicket = this.sales.map(item => Number(item.valor_total) / Number(item.qtd_vendas));
-        let sum = onlyAverageTicket.reduce((prev, next)=> Number(prev) + Number(next));
-        return this.formatToNumber(sum/onlyAverageTicket.length);
+      if (this.sales[0]) {
+        let onlyAverageTicket = this.sales.map(
+          (item) => Number(item.valor_total) / Number(item.qtd_vendas)
+        );
+        let sum = onlyAverageTicket.reduce(
+          (prev, next) => Number(prev) + Number(next)
+        );
+        return this.formatToNumber(sum / onlyAverageTicket.length);
       }
       return "0.00";
-    }
+    },
   },
   methods: {
-    formatToNumber(number){
-      return Number(number).toLocaleString('en-US',{ 
+    formatToNumber(number) {
+      return Number(number).toLocaleString("en-US", {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2,
-        useGrouping: false
+        useGrouping: false,
       });
     },
-  }
+  },
 };
 </script>
 
@@ -74,14 +84,21 @@ export default {
   <div class="divSales">
     <table class="tableSales">
       <TableHeader />
-      <TableRow v-for="sale in sales" :key="sale.loja" :sale="sale" class="salesRow"/>
-      <TableTotal :totals="{
-                          loja: 'Total',
-                          meta_valor: totalTargetSales,
-                          valor_total: totalSales,
-                          qtd_produtos: totalProductsPerSale,
-                          average_ticket: totalAverageTicket
-                           }" />
+      <TableRow
+        v-for="sale in sales"
+        :key="sale.loja"
+        :sale="sale"
+        class="salesRow"
+      />
+      <TableTotal
+        :totals="{
+          loja: 'Total',
+          meta_valor: totalTargetSales,
+          valor_total: totalSales,
+          qtd_produtos: totalProductsPerSale,
+          average_ticket: totalAverageTicket,
+        }"
+      />
     </table>
   </div>
 </template>
